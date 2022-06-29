3a3ae1fa8dc8b587ddbff3481cba1dcbf53ab81e
Matthieu Tran <matthieuktran@gmail.com>
2022-06-29 14:43:37 -0700

    Change codec definition

--- session.go
+++ session.go
@@ -4,21 +4,23 @@ import (
 	"net"
 )
 
-// A Codec is classified as a function that can take in a slice of bytes and return the manipulated form of it
-type Codec func([]byte)
-
-type Session struct {
-	id      int
-	conn    net.Conn
-	encrypt Codec
-	decrypt Codec
+// A Codec can encrypt and decrypt packets
+type Codec interface {
+	Encrypt([]byte) []byte
+	Decrypt([]byte) []byte
 }
 
-func NewSession(id int, conn net.Conn) *Session {
+type Session struct {
+	id    int
+	conn  net.Conn
+	codec Codec
+}
+
+func NewSession(id int, conn net.Conn, codec Codec) *Session {
 	return &Session{
-		id:      id,
-		conn:    conn,
-		encrypt: func([]byte) {}, decrypt: func([]byte) {},
+		id:    id,
+		conn:  conn,
+		codec: codec,
 	}
 }
 
@@ -26,21 +28,16 @@ func (s *Session) Id() int {
 	return s.id
 }
 
-// SetEncrypter changes the session's encrypter
-func (s *Session) SetEncrypter(encrypter Codec) {
-	s.encrypt = encrypter
-}
-
-// SetDecrypter changes the session's decrypter
-func (s *Session) SetDecrypter(decrypter Codec) {
-	s.decrypt = decrypter
+// SetCodec changes the session's codec
+func (s *Session) SetCodec(codec Codec) {
+	s.codec = codec
 }
 
 // Encrypt and send a slice of bytes
 func (s *Session) Write(data []byte) (int, error) {
-	s.encrypt(data)
+	res := s.codec.Encrypt(data)
 
-	return s.conn.Write(data)
+	return s.conn.Write(res)
 }
 
 // Send a slice of bytes (UNENCRYPTED)
